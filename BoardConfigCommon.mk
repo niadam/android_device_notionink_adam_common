29914ff88a46b9ab85c7011dfedac59529afa0bf
Boris Popov <bvpopov@gmail.com>
2013-01-08 20:59:32 -0800

    increase EGL cache

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,6 +89,8 @@ BOARD_USES_OVERLAY := true
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/notionink/adam_common/files/egl.cfg
 BOARD_EGL_NEEDS_LEGACY_FB := true
+MAX_EGL_CACHE_KEY_SIZE := 4096
+MAX_EGL_CACHE_SIZE := 2146304
 
 #TARGET_BOARD_INFO_FILE := device/notionink/adam_common/board-info.txt
  
