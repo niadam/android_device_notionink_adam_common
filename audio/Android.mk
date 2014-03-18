4064389f381ed54922769a77d9b52d9823db6fd8
Boris Popov <bvpopov@gmail.com>
2014-03-18 03:15:35 -0700

    Add BOARD_ADAM condition to libs

--- audio/Android.mk
+++ audio/Android.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq  ($(strip $(BOARD_ADAM)),true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -30,3 +31,4 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif
